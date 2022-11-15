1d91fef3a9b66fb990595e3005a6f041a583b8aa
Jelilmaccall <dodoojelil@gmail.com>
2022-11-15 14:22:41 -0800

    1-init_dog.c

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -3,8 +3,10 @@
 
 /**
  * init_dog - Initializes a dog structure
+ * type dog
  * @d: A dog structure.
  * @name: The name of the dog.@age: The age of the dog.
+ * @age: age of dog
  * @owner: The owner of the dog.
  *
  * Return: Nothing
